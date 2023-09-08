e1bf7099e9f43600cfe420185e1d090626be5d73
samzzii <nasstromsamuel@icloud.com>
2023-09-08 21:11:01 +0200

    Startet project

--- src/App.js
+++ src/App.js
@@ -4,23 +4,25 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  // eslint-disable-next-line no-useless-constructor
+  constructor() {
+    super();
+
+    this.state = {
+      name: {firstName: 'Samuel', lastName: 'Nasstrom'}
+    };
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1>Hello Reacter</h1>
-          <p>
-            Let's learn some React.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <p>Hi {this.state.name.firstName} {this.state.name.lastName}</p>
+          <button onClick={() => {
+            this.setState({name: {firstName: 'Gabriel', lastName: 'Michaelson'}});
+          }}>Change Name</button>
         </header>
       </div>
     );

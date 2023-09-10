260d215ea4668912b78ae4211d904397a4a06c17
samzzii <nasstromsamuel@icloud.com>
2023-09-10 23:31:56 +0200

    Added filtered search to project

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App extends Component {
 
     this.state = {
       monsters: [],
+      searchField: "",
     };
     console.log("constructor");
   }
@@ -26,24 +27,25 @@ class App extends Component {
 
   render() {
     console.log("render");
+
+    const filteredMonsters = this.state.monsters.filter(monster =>
+      monster.name.toLocaleLowerCase().includes(this.state.searchField)
+    );
+
     return (
       <div className="App">
         <input
-          className="searc-box"
+          className="search-box"
           type="search"
           placeholder="search monsters"
           onChange={(e) => {
-            const searchString = e.target.value.toLocaleLowerCase();
-            const filteredMonsters = this.state.monsters.filter((monster) => {
-              return monster.name.toLocaleLowerCase().includes(searchString);
-            });
-
+            const searchField = e.target.value.toLocaleLowerCase();
             this.setState(() => {
-              return { monsters: filteredMonsters };
-            })
+              return { searchField };
+            });
           }}
         />
-        {this.state.monsters.map((monster) => {
+        {filteredMonsters.map((monster) => {
           return (
             <div key={monster.id}>
               <h1>{monster.name}</h1>

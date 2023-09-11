5175d84036d84e34bab5827a2d6285ef67d3808b
samzzii <nasstromsamuel@icloud.com>
2023-09-11 20:27:53 +0200

    Optimized the code by adding constants for states and a method for event handler

--- src/App.js
+++ src/App.js
@@ -25,11 +25,21 @@ class App extends Component {
       );
   }
 
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchField };
+    });
+  }
+
   render() {
     console.log("render");
 
-    const filteredMonsters = this.state.monsters.filter(monster =>
-      monster.name.toLocaleLowerCase().includes(this.state.searchField)
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLocaleLowerCase().includes(searchField)
     );
 
     return (
@@ -38,12 +48,7 @@ class App extends Component {
           className="search-box"
           type="search"
           placeholder="search monsters"
-          onChange={(e) => {
-            const searchField = e.target.value.toLocaleLowerCase();
-            this.setState(() => {
-              return { searchField };
-            });
-          }}
+          onChange={ onSearchChange }
         />
         {filteredMonsters.map((monster) => {
           return (
